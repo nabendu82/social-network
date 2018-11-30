af5aa69e8d74db77b2a0469647d4249e969ddf45
Nabendu <nabendu.biswas@tesco.com>
2018-11-30 22:21:04 +0530

    Dashboard and profilev2.0

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -13,6 +13,30 @@ export const getCurrentProfile = () => dispatch => {
 //Profile loading
 export const setProfileLoading = () => {
     return { type: PROFILE_LOADING }
+}
+
+//Delete account and profile
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure?')) {
+        axios.delete('/api/profile')
+        .then(res => dispatch({ type: SET_CURRENT_USER, payload: {} }))
+        .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }))
+    }
+
+}
+
+//Add experience
+export const addExperience = (expData, history) => dispatch => {
+    axios.post('/api/profile/experience', expData)
+         .then(res => history.push('/dashboard'))
+         .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }))
+}
+
+//Add education
+export const addEducation = (eduData, history) => dispatch => {
+    axios.post('/api/profile/education', eduData)
+         .then(res => history.push('/dashboard'))
+         .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }))
 }
 
 //Create profile
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,9 @@ class Navbar extends Component {
         const authLinks = (
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
+                    <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
+                <li className="nav-item">
                     <a href="" onClick={this.onLogOutClick.bind(this)} className="nav-link">
                         <img src={user.avatar} alt={user.name} style={{ width: '25px', marginRight: '5px' }} title="Your Gravatar" />
                         Logout
