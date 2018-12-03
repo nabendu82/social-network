aaaaa561484e71dd863557e67c8b02cb22836016
Nabendu <nabendu.biswas@tesco.com>
2018-12-03 22:04:26 +0530

    ProfileDisplayv2.0

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,8 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 if(localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -46,6 +48,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
@@ -61,6 +64,7 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
