62818b3d4360186e0c7776f353ad9e9609c9ecd7
Nabendu <nabendu.biswas@tesco.com>
2018-12-03 15:47:46 +0530

    ProfileDisplayv1.0

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER, GET_PROFILES } from './types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -51,6 +51,14 @@ export const deleteEducation = (id) => dispatch => {
     axios.delete(`/api/profile/education/${id}`)
          .then(res => dispatch({ type: GET_PROFILE, payload: res.data }))
          .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }))
+}
+
+//Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get(`/api/profile/all`)
+         .then(res => dispatch({ type: GET_PROFILES, payload: res.data }))
+         .catch(err => dispatch({ type: GET_PROFILES, payload: null }))
 }
 
 //Create profile
--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from '../actions/types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -12,6 +12,8 @@ export default function(state=initialState, action) {
             return {...state, loading: true}
         case GET_PROFILE:
             return {...state, profile: action.payload, loading: false}
+        case GET_PROFILES:
+            return {...state, profiles: action.payload, loading: false}
         case CLEAR_CURRENT_PROFILE:
             return {...state, profile: null}
         default:
