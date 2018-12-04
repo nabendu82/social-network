b4564478b469af79d749477e8190f6b815273cb7
nabendu82 <nabendu.biswas@gmail.com>
2018-12-04 21:20:55 +0530

    Heroku configs

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://nabendu:welcom82@ds115613.mlab.com:15613/thesocialnetwork',
-    secretOrKey: 'thesecretoflifeis'
-};
+if(process.env.NODE_ENV === "production") {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');    
+}
