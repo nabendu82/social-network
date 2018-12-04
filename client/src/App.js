1c3e160121ca2847fe2811fa1ec5d5861945a256
Nabendu <nabendu.biswas@tesco.com>
2018-12-04 09:52:03 +0530

    PostsnCommentsv1.0

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
 
 if(localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -63,6 +64,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
